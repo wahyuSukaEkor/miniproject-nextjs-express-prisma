a66ad1c4932ca302969e5dd9d3cd46720f4dd78d
kezianafelicia <kezianaf@gmail.com>
2024-11-17 22:09:29 +0700

    feat: add frontend for feature 2.1

---
+++ apps/web/src/app/registration/registration.tsx
@@ -0,0 +1,13 @@
+import ChooseRole from "@/views/registration/chooserole";
+
+export default function Registration() {
+
+
+  return (
+    <>
+      <div>
+      <ChooseRole/>
+      </div>
+    </>
+  )
+}
