9c70428664bb065a44db4c57380a8333dc25f61a
kezianafelicia <kezianaf@gmail.com>
2024-11-22 22:26:19 +0700

    feat: create service and controller for auth

--- apps/api/src/config.ts
+++ apps/api/src/config.ts
@@ -1,14 +1,16 @@
 import { config } from 'dotenv';
 import { resolve } from 'path';
 
+
 export const NODE_ENV = process.env.NODE_ENV || 'development';
 
 const envFile = NODE_ENV === 'development' ? '.env.development' : '.env';
 
+
 config({ path: resolve(__dirname, `../${envFile}`) });
 config({ path: resolve(__dirname, `../${envFile}.local`), override: true });
 
-// Load all environment variables from .env file
 
 export const PORT = process.env.PORT || 8000;
 export const DATABASE_URL = process.env.DATABASE_URL || '';
+export const JWT_SECRET = process.env.JWT_SECRET || 'defaultsecretkey';  // Add this line
